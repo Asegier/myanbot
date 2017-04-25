e90c5067cd0f9fadae0e7dab4fb762ea28b9a775
gwonghk <gwonghk@gmail.com>
2017-04-25 14:31:52 +0800

    module exports not working

--- scripts/classroom2.js
+++ scripts/classroom2.js
@@ -1,8 +1,6 @@
-const StateMachine = require('javascript-state-machine');
-const solr = require('solr-client');
-const client = solr.createClient();
+const fsm = require('./statemachine.js')
 
-getNouns = input => {
+const getNouns = input => {
   const corenlp = require("corenlp-js-interface");
   // pass input through coreNLP for results
   let nlptext = corenlp(input,9000/*port*/,"tokenize,ssplit,pos,lemma,ner"/*annotators*/,"json"/*format*/);
@@ -22,57 +20,35 @@ getNouns = input => {
   return result
 }
 
-
-const fsm = StateMachine.create({
-  // states: greet, standby, keywordsearch, assistedsearch, recommended, chatter
-  initial: 'greet',
-  events: [
-    { name: 'goStandby',  from: ['greet', 'feedback'],  to: 'standby' },
-    { name: 'goKeywordsearch', from: 'standby', to: 'keywordsearch' },
-    { name: 'goAssistedsearch',  from: 'standby', to: 'assistedsearch' },
-    { name: 'goRecommended', from: 'standby', to: 'recommended'  },
-    { name: 'goChatter',  from: 'standby',    to: 'chatter' },
-    { name: 'goFeedback',  from: ['keywordsearch', 'assistedsearch', 'recommended', 'chatter' ], to: 'feedback' }
-  ],
-  callbacks: {
-    ongreet: (event, from, to) => {
-      return module.exports = robot => {
-        robot.hear(/(\w+)\s(\w+)/i, function(res) {
-          res.reply('current state ***: '+fsm.current);
-          var name, user;
-          res.reply("Ok, lets start");
-          fsm.goStandby();
-        });
-      }
-    },
-    onstandby: (event, from, to) => {
-      return module.exports = robot => {
-        robot.hear(/(.*)/i, res => {
-          res.reply('current state !!!!!!: '+fsm.current);
-          // return fsm.goKeywordsearch();
-        })
-      }
-    },
-    onkeywordsearch: (event, from, to) => {
-      module.exports = robot => {
-        robot.hear(/(.*)/i, res => {
-          res.send(fsm.current)
-          let input = res.match[1] // get the text inputted from user
-          let nounArr = []
-          nounArr.push(getNouns(input))
-          nounArr.push(fsm.current)
-          // convert array of words into string for output
-          // this should be wikipedia links
-          let output = nounArr.join()
-          res.send(output)
-        })
-      }
+module.exports = robot => {
+  robot.hear(/(.*)/i, res => {
+    switch (fsm.current) {
+      case `greet`:
+        res.reply('current state ***: '+fsm.current);
+        var name, user;
+        res.reply("Ok, lets start");
+        // fsm.goStandby();
+        break;
+      case `standby`:
+        res.reply('current state !!!!!!: '+fsm.current);
+        // return fsm.goKeywordsearch();
+        break;
+      case `keywordsearch`:
+        res.reply('current state !!!!!!: '+fsm.current);
+        // return fsm.goKeywordsearch();
+        // `keywordsearch`
+        res.send(fsm.current)
+        let input = res.match[1] // get the text inputted from user
+        let nounArr = []
+        nounArr.push(getNouns(input))
+        nounArr.push(fsm.current)
+        // convert array of words into string for output
+        // this should be wikipedia links
+        let output = nounArr.join()
+        res.send(output)
+        break;
+      default:
+        console.log('no fsm.current found')
     }
-
-  }
-});
-
-// robot.respond(/is it (weekend|holiday)\s?\?/i, function(msg){
-//     var today = new Date();
-//     msg.reply(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
-// })
+  })
+}
---
+++ scripts/statemachine.js
@@ -0,0 +1,63 @@
+const StateMachine = require('javascript-state-machine');
+const solr = require('solr-client');
+const client = solr.createClient();
+
+export const fsm = StateMachine.create({
+  // states: greet, standby, keywordsearch, assistedsearch, recommended, chatter
+  initial: 'greet',
+  events: [
+    { name: 'goStandby',  from: ['greet', 'feedback'],  to: 'standby' },
+    { name: 'goKeywordsearch', from: 'standby', to: 'keywordsearch' },
+    { name: 'goAssistedsearch',  from: 'standby', to: 'assistedsearch' },
+    { name: 'goRecommended', from: 'standby', to: 'recommended'  },
+    { name: 'goChatter',  from: 'standby',    to: 'chatter' },
+    { name: 'goFeedback',  from: ['keywordsearch', 'assistedsearch', 'recommended', 'chatter' ], to: 'feedback' }
+  ],
+  callbacks: {
+    ongreet: (event, from, to) => {
+      console.log('current state *********** '+fsm.current)
+      // module.exports = robot => {
+      //   debugger;
+      //   robot.hear(/(\w+)\s(\w+)/i, function(res) {
+      //     res.reply('current state ***: '+fsm.current);
+      //     var name, user;
+      //     res.reply("Ok, lets start");
+      //     fsm.goStandby();
+      //   });
+      // }
+    },
+    onenterstandby: (event, from, to) => {
+      console.log('current state !!!!!!: '+fsm.current)
+      // module.exports = robot => {
+      //   robot.hear(/(.*)/i, res => {
+      //
+      //
+      //
+      //     res.reply('current state !!!!!!: '+fsm.current);
+      //     // return fsm.goKeywordsearch();
+      //   })
+      // }
+    },
+    onkeywordsearch: (event, from, to) => {
+      // module.exports = robot => {
+      //   robot.hear(/(.*)/i, res => {
+      //     res.send(fsm.current)
+      //     let input = res.match[1] // get the text inputted from user
+      //     let nounArr = []
+      //     nounArr.push(getNouns(input))
+      //     nounArr.push(fsm.current)
+      //     // convert array of words into string for output
+      //     // this should be wikipedia links
+      //     let output = nounArr.join()
+      //     res.send(output)
+      //   })
+      // }
+    }
+
+  }
+});
+
+// robot.respond(/is it (weekend|holiday)\s?\?/i, function(msg){
+//     var today = new Date();
+//     msg.reply(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
+// })
